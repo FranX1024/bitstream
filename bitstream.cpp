ae00a8c573544a4e68a2e18af16b27ea872b775c
FranX1024 <52638847+FranX1024@users.noreply.github.com>
2019-10-25 16:36:11 +0200

    Add files via upload

---
+++ bitstream.cpp
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+/*
+Made by Fran 1024, 2019
+*/
+
+class bitStream {
+private:
+  char *data; // where bitstream is contained
+  int size;   // size of the bitstream
+public:
+  /**************************/
+  bool getbit(char x, int y) {
+    return (x >> (7 - y)) & 1;
+  }
+  int chbit(int x, int i, bool v) {
+    if(v) return x | (1 << (7 - i));
+    return x & ~(1 << (7 - i));
+  }
+  /**************************/
+
+  // opens an existing byte array as bitstream
+  void openBytes(char *bytes, int _size) {
+    data = bytes;
+    size = _size;
+  }
+  // creates a new bit stream
+  void open(int _size) {
+    data = (char*)malloc(_size);
+    size = _size;
+  }
+  // closes bit stream (frees memory)
+  void close() {
+    free(data);
+  }
+  // writes to bitstream
+  void write(int ind, int bits, int dat) {
+    ind += bits;
+    while(dat) {
+      data[ind / 8] = chbit(data[ind / 8], ind % 8, dat & 1);
+      dat /= 2;
+      ind--;
+    }
+  }
+  // reads from bitstream
+  int read(int ind, int bits) {
+    int dat = 0;
+    for(int i = ind; i <= ind + bits; i++) {
+      dat = dat * 2 + getbit(data[i / 8], i % 8);
+    }
+    return dat;
+  }
+};
